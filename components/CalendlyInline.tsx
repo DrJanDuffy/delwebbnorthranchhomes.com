cf72d7840c2e2a1cf37e56f66c39be47e35eefdd
genekellyboyle <DrDuffy@bhhsnv.com>
2026-01-22 11:06:25 -0800

    Fix CalendlyInline component to match official Calendly implementation

--- components/CalendlyInline.tsx
+++ components/CalendlyInline.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Script from 'next/script';
+import { useEffect } from 'react';
 
 type CalendlyInlineProps = {
   url: string;
@@ -13,20 +13,32 @@ export default function CalendlyInline({
   height = '700px',
   className = ''
 }: CalendlyInlineProps) {
+  useEffect(() => {
+    // Load Calendly widget script if not already loaded
+    // This matches Calendly's official implementation
+    const existingScript = document.querySelector(
+      'script[src="https://assets.calendly.com/assets/external/widget.js"]'
+    );
+
+    if (!existingScript) {
+      const script = document.createElement('script');
+      script.src = 'https://assets.calendly.com/assets/external/widget.js';
+      script.async = true;
+      script.type = 'text/javascript';
+      document.body.appendChild(script);
+    }
+  }, []);
+
   return (
     <>
-      {/* Calendly inline widget - matches official Calendly code */}
+      {/* Calendly inline widget - matches official Calendly code exactly */}
+      {/* Official format: <div class="calendly-inline-widget" data-url="..." style="min-width:320px;height:700px;"></div> */}
       {/* Inline styles are required by Calendly's widget API */}
       {/* eslint-disable-next-line react/forbid-dom-props */}
       <div
         className={`calendly-inline-widget ${className}`}
         data-url={url}
         style={{ minWidth: '320px', height }}
-      />
-      {/* Calendly widget script - loaded with async as per official code */}
-      <Script
-        src="https://assets.calendly.com/assets/external/widget.js"
-        strategy="afterInteractive"
       />
     </>
   );
